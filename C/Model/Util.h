4bf5ef48f34d7405273ec99b148dc57a251bacf7
Mathieu Baudier <mbaudier@argeo.org>
2006-05-28 20:52:42 +0000

    Introduce Model project

---
+++ C/Model/Util.h
@@ -0,0 +1,32 @@
+#pragma once
+
+//! saturates to 0 and maxValue
+inline int quickModulo( int value, int maxValue) {
+	if( value < 0 ) {
+		value += maxValue;
+	}
+	else if( value >= maxValue ) {
+		value -= maxValue;
+	}
+	return value;
+}
+
+inline int charSaturate(int value) {
+	if(value < 0) {
+		value = 0;
+	}
+	if( value > 255) {
+		value = 255;
+	}
+	return value;
+}
+
+inline int saturate(int value, int min, int max) {
+	if(value < min) {
+		value = min;
+	}
+	if(value > max) {
+		value = max;
+	}
+	return value;
+}
