294a3a3bdbf305fe8c27ffa971bc5b38de54836d
Mathieu Baudier <mbaudier@argeo.org>
2006-05-28 20:59:11 +0000

    Introduce net.sf.prenzl.stdrules project

---
+++ Java/Rules/net.sf.prenzl.stdrules/src/net/sf/prenzl/stdrules/StdRules.java
@@ -0,0 +1,20 @@
+/* Created on 7 mars 2006 */
+package net.sf.prenzl.stdrules;
+
+import net.sf.prenzl.adapter.Library;
+
+public class StdRules extends Library{
+	public static final String LIB_NAME = "stdrules";
+	
+	static{
+		System.loadLibrary(LIB_NAME);
+	}
+	
+	public StdRules()  {
+		super(LIB_NAME,TYPE_JNI);
+	} 
+
+	protected synchronized native int init(String ruleName, String topologyName,int width, int height, byte[] firstGen, String properties);
+	public native String[] listComputations();
+	public native String[] listTopologies();
+}
---
+++ Java/Rules/net.sf.prenzl.stdrules/src/net/sf/prenzl/stdrules/StdRulesPlugin.java
@@ -0,0 +1,8 @@
+package net.sf.prenzl.stdrules;
+
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+public class StdRulesPlugin  extends AbstractUIPlugin{
+
+}
