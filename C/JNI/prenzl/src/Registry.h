3a071c5b3f2bce611bd21e9269821ceb14ba44c0
Olivier Capillon <olivier.capillon@gmail.com>
2006-05-28 23:40:41 +0000

    Registry secured

--- C/JNI/prenzl/src/Registry.h
+++ C/JNI/prenzl/src/Registry.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream.h>
+#include <vector>
 
 #include "Model/Rule.h"
 #include "Model/Topology.h"
@@ -10,67 +11,92 @@ namespace Prenzl {
 	class Registry {
 	public:
 		
-		Registry() 
-		{
-			size_ = 64;
-			rules_ = new Rule*[size_];
-			topologies_ = new Topology*[size_];
-			
-			nextIndex_ = 0;
-			cout << "# REGISTRY # Initialized\n";
-			cout.flush();
+		Registry() {
+			cout << "# REGISTRY # Initialized" << std::endl;
 		}
 
-		~Registry() {
-			for(int i=0;i<nextIndex_;i++){
-				if(rules_[i]!=NULL){
-					delete rules_[i];
-				}
-				if(topologies_[i]!=NULL){
-					delete topologies_[i];
-				}
+		~Registry() {	
+			for(unsigned int i = 0; i < registerLines_.size(); i++) {
+				registerLines_[i].invalidate();
 			}
-			delete[] rules_;
-			delete[] topologies_;
-			cout << "# REGISTRY # Cleaned up\n";
-			cout.flush();
+			cout << "# REGISTRY # Cleaned up" << std::endl;
 		}
 
 		int registerRuleAndTopology(Rule * rule,Topology* topology){
 			
-			// Registers
-			int id = nextIndex_;			
-			rules_[id]=rule;
-			topologies_[id]=topology;
-			//FIXME: check size
-			nextIndex_++;
-			cout << "# REGISTRY # " << id <<"\t# Created new rule\n";
-			cout.flush();
-			return id;
+			// see if we have an invalid registerLine
+			for(unsigned int i = 0; i < registerLines_.size(); i++) {
+				if(	! registerLines_[i].isValid()) {
+					// we found an invalid line, use it
+					registerLines_[i].recycle(rule, topology);
+					cout << "# REGISTRY (RECYCLE)# " << i <<"\t# Created new rule" << std::endl;
+					return (int) i;
+				}
+			}
+			
+			// we did not find any invalid registerLine
+			// just add a new line
+			registerLines_.push_back(RegisterLine(rule, topology));
+			cout << "# REGISTRY (ADD)# " << registerLines_.size() - 1 <<"\t# Created new rule" << std::endl;
+			return (int) (registerLines_.size() - 1);
 		}
 		
 		void remove(int index){
-			delete rules_[index];
-			rules_[index]=NULL;
-			delete topologies_[index];
-			topologies_[index]=NULL;
-			cout << "# REGISTRY # " << index <<"\t# Removed rule\n";
-			cout.flush();
+			registerLines_.at(index).invalidate();
+			cout << "# REGISTRY # " << index <<"\t# Removed rule" << std::endl;
 		}
 		
 		Rule* getRule(int index){
-			return rules_[index];
+			return registerLines_.at(index).getRule();
 		}
 		Topology* getTopology(int index){
-			return topologies_[index];
+			return registerLines_.at(index).getTopology();
 		}
 
 	protected:
-		Rule** rules_;
-		Topology** topologies_;
-		
-	private:
-		int nextIndex_;
-		int size_;
+	
+		class RegisterLine {
+		public:
+			RegisterLine(Rule * rule, Topology * topology)
+			: valid_(true)
+			, rule_(rule)
+			, topology_(topology)
+			{}
+			
+			void invalidate() {
+				if(valid_) {
+					valid_ = false;
+					delete rule_;
+					delete topology_;
+					rule_ = 0;
+					topology_ = 0;
+				}
+			}
+			
+			void recycle(Rule * rule, Topology * topology) {
+				rule_ = rule;
+				topology_ = topology;
+				valid_ = true;
+			}
+			
+			bool isValid() {
+				return valid_;
+			}
+			
+			Rule * getRule() {
+				return rule_;
+			}
+			
+			Topology * getTopology() {
+				return topology_;
+			}
+			
+		private:
+			bool valid_;
+			Rule * rule_;
+			Topology * topology_;	
+		};
+	
+		std::vector<RegisterLine> registerLines_;	
 	};
 }
